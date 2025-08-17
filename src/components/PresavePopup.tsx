2f2a442980e1afcd46089accc4a46f00973b8545
hiddnhills <fondiahill@gmail.com>
2025-08-17 13:48:04 -0700

    Update PresavePopup.tsx

--- src/components/PresavePopup.tsx
+++ src/components/PresavePopup.tsx
@@ -74,7 +74,7 @@ export const PresavePopup: React.FC<PresavePopupProps> = ({
       <DialogHeader className="text-center space-y-4">
         <div className="mx-auto w-24 h-24 sm:w-32 sm:h-32 rounded-lg overflow-hidden">
           <img
-            src="https://i.imgur.com/2tgjr8J.jpeg"
+            src="https://i.imgur.com/8WoRjcb.jpeg"
             alt="Patience is a Virtue - Track Cover"
             className="w-full h-full object-cover"
             loading="lazy"
